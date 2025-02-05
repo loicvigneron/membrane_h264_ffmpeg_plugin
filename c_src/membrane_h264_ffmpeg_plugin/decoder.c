a9e05290f220f691ba4f87c962bd46937df71d03
Loic Vigneron <loic.vigneron@gmail.com>
2025-02-05 22:56:15 +0100

    Force v4l2m2m codecs

--- c_src/membrane_h264_ffmpeg_plugin/decoder.c
+++ c_src/membrane_h264_ffmpeg_plugin/decoder.c
@@ -17,7 +17,7 @@ UNIFEX_TERM create(UnifexEnv *env) {
   avcodec_register_all();
 #endif
   // const AVCodec *codec = avcodec_find_decoder(AV_CODEC_ID_H264);
-    const AVCodec *codec = avcodec_find_decoder_by_name("h264_v4l2m2m")
+    const AVCodec *codec = avcodec_find_decoder_by_name("h264_v4l2m2m");
 
   
   if (!codec) {
--- c_src/membrane_h264_ffmpeg_plugin/encoder.c
+++ c_src/membrane_h264_ffmpeg_plugin/encoder.c
@@ -25,7 +25,7 @@ UNIFEX_TERM create(UnifexEnv *env, int width, int height, char *pix_fmt,
   avcodec_register_all();
 #endif
   // const AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_H264);
-  const AVCodec *codec = avcodec_find_encoder_by_name("h264_v4l2m2m")
+  const AVCodec *codec = avcodec_find_encoder_by_name("h264_v4l2m2m");
 
   if (!codec) {
     res = create_result_error(env, "nocodec");
